bd28fccb509b4e0bc45472c366c6bb115cacf3db
Joel Southall <vextechmage@gmail.com>
2025-07-29 23:03:40 -0700

    Edit index.css to center app content

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,14 @@ import VideoExerciseCard from './components/VideoExerciseCard';
 
 function App() {
   return (
-    <div>
-      <h1 style={{ textAlign: 'center' }}>Interactive Video Exercise</h1>
-      <VideoExerciseCard />
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="p-6">
+        <h1 className="text-3xl font-bold mb-6 text-center">Interactive Video Exercise</h1>
+        <VideoExerciseCard />
+      </div>
     </div>
   );
 }
+
 
 export default App;
